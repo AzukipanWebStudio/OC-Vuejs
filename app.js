1fba6519d20cb08497118a01e7eada2a2fbef6d8
Naoto Nakashima <nao@NaotonoMac-Studio.local>
2022-12-18 15:06:06 +0900

    v-bind

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: "Hello <span style='color:blue'>Vue</span>!",
+      message: "Hello Vue again!",
     };
   },
 }).mount("#app");
