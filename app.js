939dc3f1abc20a25cee3f6f01ba2c31412247fe1
Naoto Nakashima <girasol0817@gmail.com>
2023-01-17 10:57:32 +0900

    class bind conditionally

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const { createApp } = Vue;
 
 createApp({
   data: () => ({
+    isActive: false,
     activeClass: "active",
+    errorClass: "error",
     textUnderline: "text-underline",
   }),
 }).mount("#app");
